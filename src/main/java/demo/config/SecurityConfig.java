4d64046ec3c6af738001693ee5ff1f436b362c52
bala-amypo <bala@amypo.in>
2025-12-18 04:09:16 +0000

    User commit

--- src/main/java/demo/config/SecurityConfig.java
+++ src/main/java/demo/config/SecurityConfig.java
@@ -5,10 +5,8 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
-
 @Configuration
-public class OpenApiConfig {
-
+public class SecurityConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
