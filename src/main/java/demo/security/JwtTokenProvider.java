f3fad7965c11780ed476966d0ae9432c8495921a
bala-amypo <bala@amypo.in>
2025-12-23 05:01:02 +0000

    User commit

--- src/main/java/demo/security/JwtTokenProvider.java
+++ src/main/java/demo/security/JwtTokenProvider.java
@@ -5,7 +5,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
-import java.security.Key;
+import java.security.Key; 
 import java.util.Date;
 
 @Component
