0331a73e58a0f6ef283c14878053716b38fdd243
bala-amypo <bala@amypo.in>
2025-12-23 08:26:28 +0000

    User commit

--- src/main/java/demo/config/SwaggerConfig.java
+++ src/main/java/demo/config/SwaggerConfig.java
@@ -1,19 +1,19 @@
-package com.example.demo.config;
+// package com.example.demo.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import io.swagger.v3.oas.models.OpenAPI;
+// import io.swagger.v3.oas.models.info.Info;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class SwaggerConfig {
+// @Configuration
+// public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI apiInfo() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Dynamic Event Ticket Pricing Engine")
-                        .description("API Documentation")
-                        .version("1.0"));
-    }
-}
+//     @Bean
+//     public OpenAPI apiInfo() {
+//         return new OpenAPI()
+//                 .info(new Info()
+//                         .title("Dynamic Event Ticket Pricing Engine")
+//                         .description("API Documentation")
+//                         .version("1.0"));
+//     }
+// }
